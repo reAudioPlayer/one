d186d4f87129d61d76a8f57e1a054c643f85accb
dxstiny <david.diashorta@gmail.com>
2022-10-25 18:12:22 +0200

    fix, ui: allow downloading in smart playlists (#46)

--- src/ui/src/views/Collection/Breaking.vue
+++ src/ui/src/views/Collection/Breaking.vue
@@ -1,19 +1,35 @@
 <template>
     <div class="playlist">
-        <fixed-playlist-header @click="loadPlaylist" ref="fixedHeading" :class="{ 'hidden': fixedHeaderHidden }" :title="playlistName" />
+        <fixed-playlist-header @click="loadPlaylist" ref="fixedHeading" :class="{ 'hidden': fixedHeaderHidden }"
+                               :title="playlistName"/>
         <div class="padding-20" v-observe-visibility="headerVisibilityChanged">
             <h7>Playlist</h7>
-            <h1>{{playlistName}}</h1>
-            <h5>Your {{playlist.length}} newest tracks, auto-generated just for you</h5>
+            <h1>{{ playlistName }}</h1>
+            <h5>Your {{ playlist.length }} newest tracks, auto-generated just for you</h5>
         </div>
         <hr>
         <div class="padding-20">
             <span id="loadPlaylist" @click="loadPlaylist" class="material-icons-outlined">play_circle_filled</span>
             <div class="grid">
-                <grid-header />
+                <grid-header/>
                 <hr>
                 <div class="playlistEntries">
-                    <playlist-entry v-for="(element, index) in playlist" :key="index" :index="index" :source="element.source" :id="element.id" :title="element.title" :playing="element.playing" :album="element.album" :artist="element.artist" :cover="element.cover" :favourite="element.favourite" :duration="element.duration" />
+                    <playlist-entry
+                        v-for="(element, index) in playlist"
+                        :key="index"
+                        :index="index"
+                        :source="element.source"
+                        :id="element.id"
+                        :title="element.title"
+                        :playing="element.playing"
+                        :album="element.album"
+                        :artist="element.artist"
+                        :cover="element.cover"
+                        :favourite="element.favourite"
+                        :duration="element.duration"
+                        @download="download"
+                        @requestUpdate="updateTracks"
+                    />
                 </div>
             </div>
         </div>
@@ -21,107 +37,111 @@
 </template>
 
 <script>
-    import FixedPlaylistHeader from '@/components/Playlist/FixedPlaylistHeader.vue'
-    import GridHeader from '@/components/Playlist/GridHeader.vue'
-    import PlaylistEntry from '@/components/Playlist/PlaylistEntry.vue'
-    import {mapState} from "vuex";
+import FixedPlaylistHeader from '@/components/Playlist/FixedPlaylistHeader.vue'
+import GridHeader from '@/components/Playlist/GridHeader.vue'
+import PlaylistEntry from '@/components/Playlist/PlaylistEntry.vue'
+import {mapState} from "vuex";
 
-    export default {
-        components: {
-            PlaylistEntry,
-            FixedPlaylistHeader,
-            GridHeader
+export default {
+    components: {
+        PlaylistEntry,
+        FixedPlaylistHeader,
+        GridHeader
+    },
+    data() {
+        this.updateTracks()
+        return {
+            fixedHeaderHidden: true,
+            playlist: [],
+            playlistName: "N/A"
+        }
+    },
+    computed: mapState("player", {
+        "currentSong": state => state.song.id,
+    }),
+    watch: {
+        currentSong() {
+            this.updateIsPlaying()
+        }
+    },
+    methods: {
+        download(index) {
+            const data = this.playlist?.[index]
+            window.open(`/api/tracks/${data.id}/download`)
         },
-        data() {
-            this.updateTracks()
-            return {
-                fixedHeaderHidden: true,
-                playlist: [],
-                playlistName: "N/A"
-            }
+        updateIsPlaying() {
+            console.log("Updating is playing", this.currentSong)
+            this.playlist.forEach((element) => {
+                element.playing = element.id == this.currentSong
+            })
         },
-        computed: mapState("player", {
-            "currentSong": state => state.song.id,
-        }),
-        watch: {
-            currentSong() {
-                this.updateIsPlaying()
-            }
+        headerVisibilityChanged(a) {
+            this.fixedHeaderHidden = a
         },
-        methods: {
-            updateIsPlaying() {
-                console.log("Updating is playing", this.currentSong)
-                this.playlist.forEach((element) => {
-                    element.playing = element.id == this.currentSong
+        updateTracks() {
+            fetch("/api/me/new")
+                .then(x => x.json()).then(jdata => {
+                this.playlist = jdata.songs;
+                this.playlistName = jdata.name;
+                this.updateIsPlaying();
+            })
+        },
+        loadPlaylist() {
+            fetch("/api/player/load", {
+                method: "POST",
+                body: JSON.stringify({
+                    type: "collection/breaking"
                 })
-            },
-            headerVisibilityChanged(a) {
-                this.fixedHeaderHidden = a
-            },
-            updateTracks() {
-                fetch("/api/me/new")
-                    .then(x => x.json()).then(jdata => {
-                        this.playlist = jdata.songs;
-                        this.playlistName = jdata.name;
-                        this.updateIsPlaying();
-                    })
-            },
-            loadPlaylist() {
-                fetch("/api/player/load", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        type: "collection/breaking"
-                    })
-                })
-            }
+            })
         }
     }
+}
 </script>
 
 <style scoped>
-    .playlistEntries {
-        display: flex;
-        flex-direction: column;
-    }
+.playlistEntries {
+    display: flex;
+    flex-direction: column;
+}
 
-    #loadPlaylist,
-    #addToPlaylist {
-        cursor: pointer;
-        font-size: 60px;
-        margin-bottom: 20px;
-        width: 70px;
-        line-height: 70px;
-        text-align: center;
-        align-items: center;
-        vertical-align: middle;
-    }
+#loadPlaylist,
+#addToPlaylist {
+    cursor: pointer;
+    font-size: 60px;
+    margin-bottom: 20px;
+    width: 70px;
+    line-height: 70px;
+    text-align: center;
+    align-items: center;
+    vertical-align: middle;
+}
 
-    #loadPlaylist:hover, #addToPlaylist:hover {
-        cursor: pointer;
-        font-size: 62px;
-    }
+#loadPlaylist:hover, #addToPlaylist:hover {
+    cursor: pointer;
+    font-size: 62px;
+}
 
-    .padding-20 {
-        padding: 20px;
-    }
+.padding-20 {
+    padding: 20px;
+}
 
-    h3 {
-        text-transform: uppercase;
-    }
+h3 {
+    text-transform: uppercase;
+}
 
-    h7 {
-        text-transform: uppercase;
-        font-weight: bold;
-        font-size: 0.83em;
-    }
+h7 {
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 0.83em;
+}
 
-    h1 {
-        margin-block-start: 0.15em;
-        margin-block-end: 0.15em;
-        font-size: 2.91em;
-    }
+h1 {
+    margin-block-start: 0.15em;
+    margin-block-end: 0.15em;
+    font-size: 2.91em;
+}
 
-    .hidden {
-        display: none !important;
-    }
+.hidden {
+    display: none !important;
+}
 </style>
