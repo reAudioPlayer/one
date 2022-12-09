f644f42ab794a75cf518918a31c94293e2a9faf8
dxstiny <david.diashorta@gmail.com>
2022-12-09 17:28:38 +0100

    new, ui: build update

--- src/ui/dist/assets/Tracks.b2a9c345.js
+++
@@ -1,1 +0,0 @@
-import{I as r}from"./index.53cf4f90.js";import{o,f as e,u as t}from"./index.6f3ae9d0.js";import"./FixedPlaylistHeader.ca1a986e.js";import"./PlaylistEntry.a35374d1.js";import"./EditSong.vue_vue_type_script_setup_true_lang.9a1891af.js";import"./song.22c0d5cf.js";const u={__name:"Tracks",setup(a){return(p,m)=>(o(),e(t(r),{src:"/api/me/liked"}))}};export{u as default};
