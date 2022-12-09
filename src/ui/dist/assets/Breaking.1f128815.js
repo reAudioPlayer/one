f4c4d225b23623ee5c555ec2b0ba8ee21372e551
dxstiny <david.diashorta@gmail.com>
2022-12-09 13:27:58 +0100

    new, ui: build update

--- src/ui/dist/assets/Breaking.1f128815.js
+++
@@ -1,1 +0,0 @@
-import{I as r}from"./index.b2be0aeb.js";import{o as e,f as o,u as t}from"./index.aa38e9d2.js";import"./FixedPlaylistHeader.a4c0d10c.js";import"./PlaylistEntry.d88bd703.js";import"./EditSong.vue_vue_type_script_setup_true_lang.f2d67a49.js";import"./song.91ac9cb9.js";const u={__name:"Breaking",setup(a){return(p,m)=>(e(),o(t(r),{src:"/api/me/new"}))}};export{u as default};
