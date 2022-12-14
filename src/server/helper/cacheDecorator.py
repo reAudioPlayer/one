12651b8bf5c3a3c6136a70bb43d283f42f8c7877
dxstiny <david.diashorta@gmail.com>
2022-12-14 21:46:08 +0100

    new, server: improve cache & spotify auth workflows

--- src/server/helper/cacheDecorator.py
+++ src/server/helper/cacheDecorator.py
@@ -4,32 +4,71 @@ __copyright__ = ("Copyright (c) 2022 https://github.com/reAudioPlayer")
 
 from functools import wraps
 import asyncio
-from typing import Any, Awaitable, Callable, Union
+from typing import Any, Awaitable, Callable, Union, Dict
 from aiohttp import Payload, web
+from datetime import datetime, timedelta
+
+
+Function = Callable[[Any], Awaitable[web.Response]]
+
+
+class CacheEntry:
+    """cache entry"""
+    def __init__(self,
+                 response: web.Response,
+                 expires: int) -> None:
+
+        self._response = response
+        self._expires = expires
+        self._date = datetime.now()
+
+    @property
+    def initialResponse(self) -> web.Response:
+        """returns the initial response"""
+        return self._response
+
+    def clone(self) -> web.Response:
+        """clones the response"""
+        expireStamp = self._date + timedelta(seconds = self._expires)
+        return web.Response(body = self._response.body, status = self._response.status,
+            headers={
+                **self._response.headers,
+                **{
+                    "X-Cache-Control": f"max-age={self._expires}",
+                    "X-Cache": "HIT",
+                    "X-Expires": expireStamp.strftime("%a, %d %b %Y %H:%M:%S GMT")
+                }
+            })
+
+
+_CACHE: Dict[Function, CacheEntry] = { }
+
 
 def useCache(expire: int) -> Callable[[Callable[[Any],
         Awaitable[web.Response]]], Callable[[Any, Any], Awaitable[web.Response]]]:
     """caches the response for a specified time"""
-    def _implement(function: Callable[[Any],
-            Awaitable[web.Response]]) -> Callable[[Any, Any], Awaitable[web.Response]]:
-        cache: Union[bytes, Payload, None] = None
+    def _implement(function: Function) -> Callable[[Any, Any], Awaitable[web.Response]]:
 
         @wraps(function)
         async def _wrapper(*args: Any, **kwargs: Any) -> web.Response:
-            nonlocal cache
+            if function in _CACHE:
+                print("cache hit")
+                return _CACHE[function].clone()
 
-            if cache is None:
-                print("cache empty")
-                cache = (await function(*args, **kwargs)).body
+            print("cache miss")
+            _CACHE[function] = CacheEntry(await function(*args, **kwargs), expire)
 
-                async def invalidateCache() -> None:
-                    nonlocal cache
-                    await asyncio.sleep(expire)
-                    cache = None
+            async def invalidateCache() -> None:
+                await asyncio.sleep(expire)
+                del _CACHE[function]
 
-                asyncio.create_task(invalidateCache())
-
-            assert not isinstance(cache, Payload)
-            return web.json_response(body = cache)
+            asyncio.create_task(invalidateCache())
+            return _CACHE[function].initialResponse
         return _wrapper
     return _implement
+
+def clearCache() -> None:
+    """clears the cache"""
+    global _CACHE # pylint: disable=global-statement
+    print("clearing cache")
+    _CACHE = { }
