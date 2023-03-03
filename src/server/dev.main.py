bd40c6e9972eca64d9f1da53c0551ab59ba9712c
dxstiny <david.diashorta@gmail.com>
2023-03-03 19:38:09 +0100

    fix, server: mypy/pylint

--- src/server/dev.main.py
+++ src/server/dev.main.py
@@ -4,7 +4,6 @@ __copyright__ = "Copyright (c) 2022 https://github.com/reAudioPlayer"
 
 import subprocess
 import logging
-import sys
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("main")
@@ -13,15 +12,20 @@ logger.info("init...")
 
 def run() -> None:
     """runs ./main.py"""
-    process = subprocess.Popen(["python", "-u", "main.py", "--with-docker"],
-                               shell = False,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    logger.info("process running")
+    with subprocess.Popen(["python", "-u", "main.py", "--with-docker"],
+                            shell = False,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE) as process:
+        logger.info("process running")
 
-    for c in iter(lambda: process.stdout.read(1), b""):
-        sys.stdout.buffer.write(c)
-
+        while True:
+            stdout = process.stdout
+            if stdout is None:
+                break
+            line = stdout.readline()
+            if not line:
+                break
+            logger.info(line.decode("utf-8").strip())
     logger.info("process exited")
 
 logger.info("restarting nginx...")
@@ -35,6 +39,6 @@ while True:
     try:
         logger.info("starting...")
         run()
-    except Exception as exception:
+    except Exception as exception: # pylint: disable=broad-except
         logger.exception(exception)
         logger.info("restarting...")
