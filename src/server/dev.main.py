e92245837325c891e94d977dd02b5518dd050dbc
dxstiny <david.diashorta@gmail.com>
2023-03-03 19:27:24 +0100

    new, server, artist: improve flow slightly

--- src/server/dev.main.py
+++ src/server/dev.main.py
@@ -4,6 +4,7 @@ __copyright__ = "Copyright (c) 2022 https://github.com/reAudioPlayer"
 
 import subprocess
 import logging
+import sys
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("main")
@@ -12,11 +13,16 @@ logger.info("init...")
 
 def run() -> None:
     """runs ./main.py"""
-    subprocess.run(["python", "main.py", "--with-docker"],
-                   stdout=subprocess.PIPE,
-                   stderr=subprocess.PIPE,
-                   check=True)
+    process = subprocess.Popen(["python", "-u", "main.py", "--with-docker"],
+                               shell = False,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    logger.info("process running")
 
+    for c in iter(lambda: process.stdout.read(1), b""):
+        sys.stdout.buffer.write(c)
+
+    logger.info("process exited")
 
 logger.info("restarting nginx...")
 
