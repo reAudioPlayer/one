5b6b824fbc4dd40b1a9c0a4f48189a1afcf9c21b
dxstiny <david.diashorta@gmail.com>
2023-03-09 19:56:29 +0100

    new, ui: build update

---
+++ src/ui/dist/assets/Card-1541fb70.js
@@ -0,0 +1,1 @@
+import{m as o,o as s,c as t,r,D as _,_ as c}from"./index-63840441.js";const n=o({__name:"Card",props:{withHover:{type:Boolean,default:!1}},setup(e){return(a,d)=>(s(),t("div",{class:_([{withHover:e.withHover},"card"])},[r(a.$slots,"default",{},void 0,!0)],2))}});const p=c(n,[["__scopeId","data-v-edfc457b"]]);export{p as C};
