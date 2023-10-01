296e3253b1d5f09dd9fcfbffc67ffb2d73635cb2
dxstiny <david.diashorta@gmail.com>
2023-10-01 18:56:02 +0200

    build update

--- src/ui/src/components/Catalogue/Items/Artist/ArtistItem.vue
+++ src/ui/src/components/Catalogue/Items/Artist/ArtistItem.vue
@@ -12,14 +12,20 @@
                     id,
                     name,
                     href: `https://open.spotify.com/artist/${id}`,
-                    image: cover
+                    image: cover,
                 }"
             />
             <div class="item">
                 <img :src="cover" />
-                <h4>{{name}}</h4>
+                <h4>{{ name }}</h4>
                 <p v-html="description" />
-                <button v-if="showFollowButton" class="followButton" @click="follow">{{following ? "Following" : "Follow"}}</button>
+                <button
+                    v-if="showFollowButton"
+                    class="followButton"
+                    @click.stop="follow"
+                >
+                    {{ following ? "Following" : "Follow" }}
+                </button>
             </div>
         </Card>
     </router-link>
@@ -29,101 +35,99 @@
 import Card from "@/containers/Card.vue";
 
 export default {
-        components: { Card },
-        name: 'ArtistItem',
-        methods: {
-            follow(e)
-            {
-                e.stopPropagation();
-                const method = this.following ? "DELETE" : "POST"
-                fetch("/api/spotify/following", {
-                        method,
-                        body: JSON.stringify({
-                            "artistId": this.id
-                        })
-                    }).then(x => {
-                        if (x.status == 200)
-                        {
-                            this.following = !this.following
-                        }
-                    })
-            }
+    components: { Card },
+    name: "ArtistItem",
+    methods: {
+        follow(e) {
+            e.stopPropagation();
+            const method = this.following ? "DELETE" : "POST";
+            fetch("/api/spotify/following", {
+                method,
+                body: JSON.stringify({
+                    artistId: this.id,
+                }),
+            }).then((x) => {
+                if (x.status == 200) {
+                    this.following = !this.following;
+                }
+            });
         },
-        data() {
-            return {
-                following: false
-            }
-        },
-        props: {
-            cover: String,
-            name: String,
-            description: String,
-            id: String,
-            showFollowButton: Boolean
-        }
-    }
+    },
+    data() {
+        return {
+            following: false,
+        };
+    },
+    props: {
+        cover: String,
+        name: String,
+        description: String,
+        id: String,
+        showFollowButton: Boolean,
+    },
+};
 </script>
 
 <style scoped>
-    .followButton {
-        color: var(--fg-base);
-        background: none;
-        border: 1px solid var(--border-base);
-        border-radius: 2px;
-        font-family: var(--ff-base);
-        text-transform: uppercase;
-        margin-top: 5px;
-        padding: 5px 10px;
-        font-weight: bold;
-        letter-spacing: 1px;
-    }
+.followButton {
+    color: var(--fg-base);
+    background: none;
+    border: 1px solid var(--border-base);
+    border-radius: 2px;
+    font-family: var(--ff-base);
+    text-transform: uppercase;
+    margin-top: 5px;
+    padding: 5px 10px;
+    font-weight: bold;
+    letter-spacing: 1px;
+}
 
-    .followButton:hover {
-        cursor: pointer;
-    }
+.followButton:hover {
+    cursor: pointer;
+}
 
-    p.note {
-        font-size: .7em;
-    }
+p.note {
+    font-size: 0.7em;
+}
 
-    .wrapper {
-        background: var(--bg-base-lt);
-        border-radius: 20px;
-        min-height: 10vh;
-        margin: 10px;
-        padding: 20px;
-        position: relative;
-        text-decoration: none !important;
-        height: calc(100% - 20px);
-    }
+.wrapper {
+    background: var(--bg-base-lt);
+    border-radius: 20px;
+    min-height: 10vh;
+    margin: 10px;
+    padding: 20px;
+    position: relative;
+    text-decoration: none !important;
+    height: calc(100% - 20px);
+}
 
-    .wrapper:hover {
-        cursor: pointer;
-        background: var(--bg-hover-dk);
-    }
+.wrapper:hover {
+    cursor: pointer;
+    background: var(--bg-hover-dk);
+}
 
-    .item {
-        height: calc(100% - 40px);
-    }
+.item {
+    height: calc(100% - 40px);
+}
 
-    img {
-        width: 100%;
-        border-radius: 12px;
-        margin-bottom: 1em;
-    }
+img {
+    width: 100%;
+    border-radius: 12px;
+    margin-bottom: 1em;
+}
 
-    h4 {
-        margin: 0;
-    }
+h4 {
+    margin: 0;
+}
 
-    p {
-        margin: 0;
-        color: var(--fg-base-dk);
-        font-size: .8em;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
+p {
+    margin: 0;
+    color: var(--fg-base-dk);
+    font-size: 0.8em;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
 </style>
